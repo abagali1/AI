2fba183406a36c705b09ea8c3fef8b6324b29635
Anup Bagali <anup.bagali1@gmail.com>
2019-09-10 16:46:32 -0400

    Fix infinite looping error & cleanup

--- SEM1/slider_lab/slider.py
+++ SEM1/slider_lab/slider.py
@@ -4,14 +4,13 @@ import time
 
 def backtrack(visited_nodes, goal):
     if len(visited_nodes) == 0:
-        return [goal]
+        return [goal], 0
     path = [visited_nodes[goal]]
-    path.extend(visited_nodes[i] for i in path if i in visited_nodes.keys())
+    for i in path:
+        if i in visited_nodes.keys() and visited_nodes[i] != '':
+            tmp = visited_nodes[i]
+            path.append(tmp)
     return path[::-1], len(path)
-
-
-def to_string(matrix):
-    return [(''.join(str(item) for inList in m for item in inList)) for m in matrix]
 
 
 def gen_swaps(p, n, i):
@@ -24,12 +23,11 @@ def gen_swaps(p, n, i):
 
 
 def get_children(parent):
-    print("PARENT: " + str(parent))
     index = [(index, row.index('_')) for index, row in enumerate(parent) if '_' in row][0]
     neighbors = [(x, y) for x, y in [(index[0], index[1] - 1), (index[0], index[1] + 1), (index[0] + 1, index[1]),
                                      (index[0] - 1, index[1])] if
                  0 <= x < len(parent) and 0 <= y < len(parent[0])]
-    return to_string(gen_swaps(parent, neighbors, index))
+    return [(''.join(str(item) for inList in m for item in inList)) for m in gen_swaps(parent, neighbors, index)]
 
 
 def solve(puzzle, goal="12345678_"):
@@ -40,15 +38,15 @@ def solve(puzzle, goal="12345678_"):
 
     while parent:
         elem = parent.pop(0)
-        # neighbors = get_children(elem)
-        print(f"ELEM: {str(elem)}")
         neighbors = get_children([list(elem[0:3]), list(elem[3:6]), list(elem[6:9])])
         for n in neighbors:
             if n == goal:
                 visited[''.join([str(item) for i in n for item in i])] = elem
                 return backtrack(visited, goal)
-            visited[''.join([str(item) for i in n for item in i])] = elem
-            parent.extend(neighbors)
+            elif n not in visited.keys():
+                visited[''.join([str(item) for i in n for item in i])] = elem
+                parent.append(n)
+    return ['NULL'], -1
 
 
 def main():
@@ -65,8 +63,13 @@ def main():
         goal = None
 
     start_time = time.time()
-    print(f"Steps: {solve(puzzle, goal)}")
-    print(f"Time: {time.time() - start_time}")
+    if not goal:
+        solved = solve(puzzle)
+        print("Steps: %d" % solved[1])
+    else:
+        solved = solve(puzzle, goal)
+        print("Steps: %d" % solved[1])
+    print("Time: %ds" % (time.time() - start_time))
 
 
 if __name__ == '__main__':
