2d97929797400f8ff13006692f30071c802331ad
Anup Bagali <anup.bagali1@gmail.com>
2019-09-15 16:54:45 -0400

    python3

--- SEM1/lab3/coding_bat_3.py
+++ SEM1/lab3/coding_bat_3.py
@@ -55,7 +55,7 @@ def lucky_sum(a, b, c):
 def no_teen_sum(a, b, c):
   return sum([x for x in [a,b,c] if x>19 or x<13 or x==15 or x==16 ])
 def round_sum(a, b, c):
-  return sum([x-x%10 if x%10<5 else x+(10-x%10) for x in [a,b,c] ])
+  return sum([x-x%10 for x in [a,b,c] if x%10<5]) + sum([x+(10-x%10) for x in [a,b,c] if not x%10<5])
 def close_far(a, b, c):
   return (abs(b-a)<=1 and abs(c-a)>=2 and abs(c-b) >=2) or (abs(c-a)<=1 and abs(b-a)>=2 and abs(b-c)>=2)
 def make_chocolate(small, big, goal):
