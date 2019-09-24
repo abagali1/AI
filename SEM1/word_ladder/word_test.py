a58e63d612fa4cf4e98138c127ad8f3064df03b0
Anup Bagali <anup.bagali1@gmail.com>
2019-09-24 19:29:17 -0400

    actually try

--- SEM1/word_ladder/word_test.py
+++ SEM1/word_ladder/word_test.py
@@ -11,23 +11,31 @@ def degree_list(graph):
 
 def create_graph(start, words):
     graph = {}
+
     for i in words:
-        graph[i] = []
+        graph[i] = set()
+        e = 0
+        wl = list(i)
         for j in range(len(i)):
-            for a in "abcdefghijklmnopqrstuvwxyz":
-                new_word = i[:j] + a + i[j + 1:]
-                if new_word in words and new_word != i:
-                    graph[i].append(new_word)
-                    if new_word in graph.keys():
-                        graph[new_word].append(i)
+            original = wl[j]
+            for letter in "abcdefghijklmnopqrstuvwxyz":
+                wl[j] = letter
+                nw = ''.join(wl)
+                if nw in words:
+                    graph[i].add(nw)
+                    if nw in graph.keys():
+                        graph[nw].add(i)
                     else:
-                        graph[new_word] = [i]
+                        graph[nw] = {i}
+                    e += 1
+                wl[j] = original
 
     end = time()-start
     print("Word count: {0}".format(str(len(words))))
-    print("Edge count: {0}".format(
-        str(sum([len(graph[i]) for i in graph.keys()]))))
-    print("Degree list: {0}".format(degree_list(graph)))
+    print("Edge count: {0}".format(e))
+    #print("Edge count: {0}".format(
+    #    str(sum([len(graph[i]) for i in graph.keys()]))))
+    #print("Degree list: {0}".format(degree_list(graph)))
     print("Construction time: %.2lfs" % end)
 
     return graph
