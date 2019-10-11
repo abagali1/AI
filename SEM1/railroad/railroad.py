201680f9417955dabddb18ae4eae797736a16d14
Anup Bagali <anup.bagali1@gmail.com>
2019-10-11 08:06:20 -0400

    added great circle distance

--- SEM1/railroad/railroad.py
+++ SEM1/railroad/railroad.py
@@ -1,18 +1,21 @@
 from sys import argv
+from math import sin, cos, acos, pi
 
-names = {} # stores station code -> name
-graph = {} # station code -> [ name, (lat,long), [neighbors] ]
+names = {}  # name -> station code
+graph = {}  # station code -> [ (lat,long), [neighbors] ]
 
 
 def load_table():
     global names, graph
-    names = {' '.join(x.split(" ")[1:]): x.split(" ")[0] for x in open('rrNodeCity.txt').read().splitlines()}
+    names = {' '.join(x.split(" ")[1:]): x.split(" ")[0]
+             for x in open('rrNodeCity.txt').read().splitlines()}
     for x in open('rrNodes.txt').read().splitlines():
         parts = x.split(" ")
-        graph[int(parts[0])] = [(float(parts[1]), float(parts[2])), []]
+        graph[parts[0]] = [(float(parts[1]), float(parts[2])), []]
     for x in open("rrEdges.txt").read().splitlines():
         parts = x.split(" ")
-        graph[int(parts[0])][2].append(int(parts[1]))
+        graph[parts[0]][1].append(parts[1])
+        graph[parts[1]][1].append(parts[0])
 
 
 def a_star(cities):
@@ -27,11 +30,16 @@ def a_star(cities):
             elif dest == '':
                 dest = tmp.strip()
             tmp = ''
-    print(f"ROOT: {root}, DEST: {dest}")
+    print(f"ROOT: {names[root]} -> {graph[names[root]]}")
+    print(f"DEST: {names[dest]} -> {graph[names[dest]]}")
 
 
-
-
+def h(x1, y1, x2, y2):
+    x1 *= pi/180.0
+    y1 *= pi/180.0
+    x2 *= pi/180.0
+    y2 *= pi/180.0
+    return acos(sin(y1)*sin(y2) + cos(y1)*cos(y2)*cos(x2-x1)) * 3958.76
 
 
 def main():
