502b504d89c69a44e399a57ec26b7c44cad8b305
Anup Bagali <anup.bagali1@gmail.com>
2019-10-12 15:34:15 -0400

    railroad a*

--- SEM1/railroad/railroad.py
+++ SEM1/railroad/railroad.py
@@ -5,6 +5,14 @@ names = {}  # name -> station code
 graph = {}  # station code -> [ (lat,long), [neighbors] ]
 
 
+def backtrack(visited_nodes, goal):
+    path = [visited_nodes[goal]]
+    for i in path:
+        if i in visited_nodes.keys() and visited_nodes[i] != '':
+            path.append(visited_nodes[i])
+    return path
+
+
 def load_table():
     global names, graph
     names = {' '.join(x.split(" ")[1:]): x.split(" ")[0]
@@ -26,15 +34,30 @@ def a_star(cities):
         tmp += cities[x] + " "
         if tmp.strip() in names:
             if root == '':
-                root = tmp.strip()
+                root = names[tmp.strip()]
             elif dest == '':
-                dest = tmp.strip()
+                dest = names[tmp.strip()]
             tmp = ''
-    print(f"ROOT: {names[root]} -> {graph[names[root]]}")
-    print(f"DEST: {names[dest]} -> {graph[names[dest]]}")
+
+    open_set, closed_set = [], {}
+    h = gcd(graph[root][0][0], graph[root][0][1], graph[dest][0][0], graph[dest][0][1])
+    open_set.append((0, h, root, ''))
+    while open_set:
+        elem = open_set.pop(0)
+        if elem in closed_set:
+            continue
+        closed_set[elem] = elem[3]
+        for nbr in graph[elem[2]][1]:
+            if nbr == dest:
+                return backtrack(closed_set, dest)
+            else:
+                h = gcd(graph[elem[2]][0][0], graph[elem[2]][0][1], graph[nbr][0][0], graph[nbr][0][1])
+                open_set.append((elem[0]+1, h, nbr, elem[3]))
+        if elem[0] + elem[1] != open_set[0][0] + open_set[0][1]:
+            open_set.sort(key=lambda a: a[0]+a[1])
 
 
-def h(x1, y1, x2, y2):
+def gcd(x1, y1, x2, y2):
     x1 *= pi/180.0
     y1 *= pi/180.0
     x2 *= pi/180.0
@@ -45,7 +68,7 @@ def h(x1, y1, x2, y2):
 def main():
     cities = argv[1:]
     load_table()
-    a_star(cities)
+    print(a_star(cities))
 
 
 if __name__ == '__main__':
