30186473fafba90331e9d6561d2d7d54385f04c0
Anup Bagali <anup.bagali1@gmail.com>
2019-10-23 00:21:17 -0400

    started sudoku

---
+++ SEM1/constraint_programming/sudoku/sudoku.py
@@ -0,0 +1,42 @@
+from sys import argv
+
+
+def string_to_pzl(p, dim, size):
+    pzls = []
+    
+
+
+
+def is_invalid(p, q):
+    hex_dict = string_to_pzl(p)
+    for hex in range(len(hex_dict)):
+        for i in range(1, 7):
+            if hex_dict[hex].count(str(i)) > 1:
+                return True
+    return False
+
+
+def is_solved(pzl, question):
+    return False if pzl.find(".") != -1 else True
+
+
+def brute_force(pzl, question):
+    if is_invalid(pzl, question):
+        return ""
+    if is_solved(pzl, question):
+        return pzl
+
+    i = pzl.find(".")
+    new_pzls = [pzl[:i] + str(j) + pzl[i + 1:] for j in nums[question]]
+    for new_pzl in new_pzls:
+        b_f = brute_force(new_pzl, question)
+        if b_f:
+            return b_f
+
+
+if __name__ == '__main__':
+    sol = brute_force(argv[1])
+    if sol is None:
+        print("No Solution Possible")
+    else:
+        print(sol)
