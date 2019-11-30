e7ccd6d6d2297f481187b5ab332b74b03d33b589
Anup Bagali <anup.bagali1@gmail.com>
2019-11-30 17:25:16 -0500

    started sumn

--- SEM1/constraint_programming/blocks/blocks.py
+++ SEM1/constraint_programming/blocks/blocks.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+from sys import argv
+
+
+def clean_args(args):
+    pzl = (int(args[0][:args[0].find('x')]), int(args[0][args[0].find('x')+1:])) if 'x' in args[0] else (int(args[0]), int(args[1]))
+    blocks = []
+    start = 1 if 'x' in args[0] else 2
+    print(args[start:])
+
+    for i in args[start:]:
+        if 'x' in i:
+            blocks.append((int(i.split('x')[0]), int(i.split('x')[1])))
+            args.remove(i)
+    
+    blocks += [(int(args[i]), int(args[i+1])) for i in range(start, len(args), 2)]
+        
+
+    return '.'*(pzl[0]*pzl[1]), blocks
+
+
+
+def brute_force(pzl, blocks):
+    if is_invalid(pzl):
+        return ""
+    if not blocks:
+        return pzl
+
+
+
+
+def main():
+    pzl, blocks = clean_args(argv[1:])
+    sol = brute_force(pzl, blocks)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
