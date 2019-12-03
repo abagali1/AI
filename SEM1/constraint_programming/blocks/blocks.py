c0f31ca63e57b3161e2802acdfbbc14588b5bf30
Anup Bagali <anup.bagali1@gmail.com>
2019-12-03 12:01:00 -0500

    ready for brute_force

--- SEM1/constraint_programming/blocks/blocks.py
+++ SEM1/constraint_programming/blocks/blocks.py
@@ -48,29 +48,32 @@ def to_string(pzl):
     return '\n'.join([''.join([pzl[i*PZL_WIDTH +j][0] for j in range(PZL_WIDTH)]) for i in range(PZL_HEIGHT)]).strip()
 
 
+def decomposition(pzl):
+    return ""
+
 def main():
-    global LETTERS, PZL_HEIGHT, PZL_WIDTH, PZL_PARITY
+    global LETTERS, PZL_HEIGHT, PZL_WIDTH, PZL_PARITY, PZL_AREA, PZL_LONGEST_SIDE 
 
     args = ' '.join(argv[1:]).lower().replace('x',' ').split(" ")  # standardize format(no more 'x')
     pzl, blocks = '.' * int(args[0])*int(args[1]), sorted([(int(args[i]), int(args[i+1]), ALPHABET[pos]) for pos, i in enumerate(range(2, len(args), 2))],key=lambda x: x[0]*x[1]) # extract blocks
     PZL_HEIGHT, PZL_WIDTH = int(args[0]), int(args[1])
     PZL_PARITY, PZL_AREA = (PZL_HEIGHT + PZL_WIDTH) % 2, len(pzl)
     PZL_LONGEST_SIDE = PZL_HEIGHT if PZL_HEIGHT > PZL_WIDTH else PZL_WIDTH
+    LETTERS = {i[2]: (i[0], i[1]) for i in blocks}
 
 
     if not can_fit(pzl, blocks):
         return "No solution"
 
     if len(blocks) == 1:
-        return "Decomposition: {0}x{1}".format(blocks[0][0], blocks[0][1]) if blocks[0][0] == pzl.height \
+        return "Decomposition: {0}x{1}".format(blocks[0][0], blocks[0][1]) if blocks[0][0] == PZL_HEIGHT \
             else "Decomposition: {0}x{1}".format(blocks[0][1], blocks[0][0])
 
 
-    LETTERS = {i[2]: (i[0], i[1]) for i in blocks}
 
     sol = brute_force(pzl=pzl, blocks=blocks)
     if sol:
-        return "Decomposition: {0}".format(pzl.decomposition)
+        return "Decomposition: {0}".format(decomposition(pzl))
 
     return "No solution"
 
