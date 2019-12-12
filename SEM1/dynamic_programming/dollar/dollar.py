f5dda072e1023dff118ad5d459c3ea65d164aa33
Anup Bagali <anup.bagali1@gmail.com>
2019-12-12 10:57:12 -0500

    dp change

--- SEM1/dynamic_programming/dollar/dollar.py
+++ SEM1/dynamic_programming/dollar/dollar.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 from sys import argv
 
+CACHE = {}
+seen = set()
 
 def change(total, coins):
-  return total, coins
+  if(total < 0):
+    return 0
+  if(total == 0):
+    return 1
 
+  if total not in CACHE:
+    CACHE[total] = sum([change(total-x, coins) for x in coins])
+    print(CACHE[total])
+  return CACHE[total]
 
 
 val = int(argv[1])
 coins = [int(x) for x in argv[2:]]
+
 print(change(val,coins))
