2e31eafe50a1b46a1d6241de05dfcfaeb26361ed
Anup Bagali <anup.bagali1@gmail.com>
2019-12-12 10:56:54 -0500

    sudoku god

---
+++ SEM1/constraint_programming/sudoku/sudoku_2C_trial.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+# Author: Ali Assaf <ali.assaf.mail@gmail.com>
+# Copyright: (C) 2010 Ali Assaf
+# License: GNU General Public License <http://www.gnu.org/licenses/>
+
+from sys import argv
+from time import process_time as time
+from math import sqrt
+
+def product(*args, repeat=1):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
+
+
+def solve_sudoku(size, grid):
+    """ An efficient Sudoku solver using Algorithm X.
+
+    >>> grid = [
+    ...     [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    ...     [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    ...     [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    ...     [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    ...     [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    ...     [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    ...     [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    ...     [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    ...     [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    >>> for solution in solve_sudoku((3, 3), grid):
+    ...     print(*solution, sep='\\n')
+    [5, 3, 4, 6, 7, 8, 9, 1, 2]
+    [6, 7, 2, 1, 9, 5, 3, 4, 8]
+    [1, 9, 8, 3, 4, 2, 5, 6, 7]
+    [8, 5, 9, 7, 6, 1, 4, 2, 3]
+    [4, 2, 6, 8, 5, 3, 7, 9, 1]
+    [7, 1, 3, 9, 2, 4, 8, 5, 6]
+    [9, 6, 1, 5, 3, 7, 2, 8, 4]
+    [2, 8, 7, 4, 1, 9, 6, 3, 5]
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    """
+    R, C = size
+    N = R * C
+    X = ([("rc", rc) for rc in product(range(N), range(N))] +
+         [("rn", rn) for rn in product(range(N), range(1, N + 1))] +
+         [("cn", cn) for cn in product(range(N), range(1, N + 1))] +
+         [("bn", bn) for bn in product(range(N), range(1, N + 1))])
+    Y = dict()
+    for r, c, n in product(range(N), range(N), range(1, N + 1)):
+        b = (r // R) * R + (c // C) # Box number
+        Y[(r, c, n)] = [
+            ("rc", (r, c)),
+            ("rn", (r, n)),
+            ("cn", (c, n)),
+            ("bn", (b, n))]
+    X, Y = exact_cover(X, Y)
+    for i, row in enumerate(grid):
+        for j, n in enumerate(row):
+            if n:
+                select(X, Y, (i, j, n))
+    for solution in solve(X, Y, []):
+        for (r, c, n) in solution:
+            grid[r][c] = n
+        yield grid
+
+def exact_cover(X, Y):
+    X = {j: set() for j in X}
+    for i, row in Y.items():
+        for j in row:
+            X[j].add(i)
+    return X, Y
+
+def solve(X, Y, solution):
+    if not X:
+        yield list(solution)
+    else:
+        c = min(X, key=lambda c: len(X[c]))
+        for r in list(X[c]):
+            solution.append(r)
+            cols = select(X, Y, r)
+            for s in solve(X, Y, solution):
+                yield s
+            deselect(X, Y, r, cols)
+            solution.pop()
+
+def select(X, Y, r):
+    cols = []
+    for j in Y[r]:
+        for i in X[j]:
+            for k in Y[i]:
+                if k != j:
+                    X[k].remove(i)
+        cols.append(X.pop(j))
+    return cols
+
+def deselect(X, Y, r, cols):
+    for j in reversed(Y[r]):
+        X[j] = cols.pop()
+        for i in X[j]:
+            for k in Y[i]:
+                if k != j:
+                    X[k].add(i)
+
+if __name__ == "__main__":
+    start = time()
+    with open(argv[1]) as f:
+        lines = f.readlines()
+        lines = list(map(str.strip, lines))
+        size = 0
+        for index,line in enumerate(lines):
+            if len(line) != size:
+                dim = int(sqrt(len(line)))
+                sub_pzl_row = int(sqrt(dim))
+                sub_pzl_col = dim // sub_pzl_row
+
+            grid = [list(line[i:i + dim]) for i in range(0, len(line), dim)]
+            for i in grid:
+                for ind, j in enumerate(i):
+                    i[ind] = int(j,size) if j != "." else 0
+
+            print("Pzl: {0}".format(index))
+            print(line)
+            print("".join("".join(map(str, x)) for x in next(solve_sudoku((sub_pzl_row, sub_pzl_col), grid))))
+            print("405")
