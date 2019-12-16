81f56f87fbd2df0d45024bb29d3642ce3cc0f1cd
Anup Bagali <anup.bagali1@gmail.com>
2019-12-16 09:43:20 -0500

    stuffs

--- SEM1/games/tictactoe/possibilities.py
+++ SEM1/games/tictactoe/possibilities.py
@@ -11,6 +11,9 @@ INDICES, INDICES_2D ={}, {}
 CONSTRAINTS = []
 all_boards = set()
 x_boards, o_boards, tie = set(), set(), set()
+terminal_board = set()
+GAMES = {}
+game = 0
 
 
 
@@ -44,62 +47,71 @@ def generate_constraints():
     CONSTRAINTS.append(diag)
 
 def finished(pzl):
+    if '.' not in pzl:
+        return 'T'
     for i in CONSTRAINTS:
         con = [pzl[j] for j in i]
-        if con.count(X) >= WIN:
-            x_boards.add(pzl)
-            return True
-        if con.count(O) >= WIN:
-            print(pzl)
-            o_boards.add(pzl)
-            return True
+        if con.count(X) == WIN:
+            return 'X'
+        if con.count(O) == WIN:
+            return 'O'
     return False
 
-def possibilities(pzl, piece):
-    all_boards.add(pzl)
-    if pzl in CACHE:
-        return 0
-    if '.' not in pzl:
-        CACHE.add(pzl)
-        tie.add(pzl)
-        return 1
-    if finished(pzl):
-        CACHE.add(pzl)
-        return 1
-    set_of_choices = [pos for pos,elem in enumerate(pzl) if elem == '.']
-    t = 0
-    tmp = [*pzl]
-    piece = X if piece == O else O
-    for index in set_of_choices:
-        tmp[index] = piece
-        new_pzl = ''.join(tmp)
-        if new_pzl not in all_boards:
-            t += possibilities(''.join(tmp),piece )
-        tmp[index] = '.'
-    return t
-
-
-# @lru_cache(maxsize=None)        
 # def possibilities(pzl, piece):
-#     global all_boards
-
-#     if finished(pzl):
-#         all_boards.add(pzl)
+#     all_boards.add(pzl)
+#     if pzl in CACHE:
+#         return 0
+#     if '.' not in pzl:
+#         CACHE.add(pzl)
+#         tie.add(pzl)
 #         return 1
-
-#     boards = []
-#     tmp = [*pzl]
-#     for pos, elem in enumerate(pzl):
-#         if not elem == ".":
-#             continue
-#         b = place(tmp, pos, piece)
-#         boards.append(b)
-
-#     piece = X if piece == O else O
+#     if finished(pzl):
+#         CACHE.add(pzl)
+#         return 1
+#     set_of_choices = [pos for pos,elem in enumerate(pzl) if elem == '.']
 #     t = 0
-#     for b in boards:
-#         t += possibilities(b, piece)
+#     tmp = [*pzl]
+#     piece = X if piece == O else O
+#     for index in set_of_choices:
+#         tmp[index] = piece
+#         new_pzl = ''.join(tmp)
+#         if new_pzl not in all_boards:
+#             t += possibilities(''.join(tmp),piece )
+#         tmp[index] = '.'
 #     return t
+
+
+def possibilities(pzl, piece):
+    global game
+    all_boards.add(pzl)
+    if pzl not in CACHE:
+        b = [*pzl]
+        f = finished(pzl)
+        if f:
+            terminal_board.add(pzl)
+            if f == 'X':
+                x_boards.add(pzl)
+            if f == 'O':
+                o_boards.add(pzl)
+            if f == 'T':
+                tie.add(pzl)
+            return 1
+        set_of_choices = [i for i in range(AREA) if pzl[i] == '.']
+        CACHE.add(pzl)
+        GAMES[pzl] = len(set_of_choices)
+        t = 0
+        new_piece = X if piece == O else O
+        for choice in set_of_choices:
+            b[choice] = piece
+            new_pzl = ''.join(b)
+            b[choice] = '.'
+            t += possibilities(new_pzl,new_piece)
+        return t
+    else:
+        game += GAMES[pzl]
+        return 0
+
+
 
 
 
@@ -122,7 +134,7 @@ def main():
     print("X: {0}".format(len(x_boards)))
     print("O: {0}".format(len(o_boards)))
     print("Tie: {0}".format(len(tie)))
-    print("Games: {0}".format(games))
+    print("Games: {0}".format(game))
 
 
 if __name__ == "__main__":
