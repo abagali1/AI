68539dfb340003d37d7384b820d8eb6ab3c93301
Anup Bagali <anup.bagali1@gmail.com>
2020-01-06 18:58:34 -0500

    use LSB to calculate on bits

--- SEM1/games/othello/lab1/othello_bitboard.py
+++ SEM1/games/othello/lab1/othello_bitboard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from sys import argv
 from time import time as time
+from math import log2
 
 # << = positive ///\\\ >>=negative   
 #     NW     N     NE 
@@ -30,8 +31,6 @@ MASKS = { #TODO: Make these all single numbers
     -9: lambda x: ((x & 9187201950435737471) << 1)>>8,
     -7: lambda x: ((x & 18374403900871474942) >> 1)>>8
 }
-
-
 # MASKS = { # FIXME: fix these numbers
 #     1: 18374403900871474942,        
 #     -1: 9187201950435737471,    
@@ -43,6 +42,7 @@ MASKS = { #TODO: Make these all single numbers
 #     -9: 35887507618889599
 # }   
 
+LOG = {1<<i:i for i in range(64)}
 
 
 def bit_not(x):
@@ -63,9 +63,14 @@ def fill(current, opponent, direction):
 
 def possible_moves(board, piece):
     final = 0b0
+    possible = set()
     for d in MASKS:
         final |= fill(board[piece], board[not piece], d) & (18446744073709551615 - (board[piece]|board[not piece]))
-    return {pos for pos,elem in enumerate('{:064b}'.format(final)) if elem == '1'}    
+    while(final):
+        b = final & -final
+        possible.add(LOG[b])
+        final -= b
+    return possible  
 
 def to_string(b):
     return '\n'.join(
