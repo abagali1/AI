a8b484528d6dde71e6f8dfe2e7be77fea4b9060d
Anup Bagali <anup.bagali1@gmail.com>
2020-01-23 12:00:18 -0500

    add tournament

---
+++ games/othello/lab6/tmp.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+from sys import argv
+from time import time 
+
+FULL_BOARD = 0xffffffffffffffff
+MASKS = {
+    -1: 0xfefefefefefefefe,
+    1: 0x7f7f7f7f7f7f7f7f,
+    8: FULL_BOARD,
+    -8: FULL_BOARD,
+    7: 0xfefefefefefefefe,
+    9: 0x7f7f7f7f7f7f7f7f,
+    -7: 0x7f7f7f7f7f7f7f7f,
+    -9: 0xfefefefefefefefe
+}
+
+
+MOVES = {i: 1 << (63 - i) for i in range(64)}
+POS = {MOVES[63 - i]: 63 - i for i in range(64)}
+
+
+HAMMING_CACHE = {}
+POSSIBLE_CACHE = {(68853694464, 34628173824, 0): {34, 43, 20, 29}, (68853694464, 34628173824, 1): {26, 19, 44, 37}}
+TREE_CACHE = {}
+
+
+def string_to_board(board):
+    return {
+        0: int(board.replace(".","0").replace("O","1").replace("X","0"),2),
+        1: int(board.replace(".","0").replace("O","0").replace("X","1"),2)
+    }
+
+def hamming_weight(n):
+    if n in HAMMING_CACHE:
+        return HAMMING_CACHE[n]
+    else:
+        orig = n
+        c = 0
+        while n:
+            c+=1
+            n ^= n&-n
+        HAMMING_CACHE[orig] = c
+        return HAMMING_CACHE[orig]
+
+
+def fill(current, opponent, direction):
+    mask = MASKS[direction]
+    if direction > 0:
+        w = ((current & mask) << direction) & opponent
+        w |= ((w & mask) << direction) & opponent
+        w |= ((w & mask) << direction) & opponent
+        w |= ((w & mask) << direction) & opponent
+        w |= ((w & mask) << direction) & opponent
+        w |= ((w & mask) << direction) & opponent
+        return (w & mask) << direction
+    else:
+        direction *= -1
+        w = ((current & mask) >> direction) & opponent
+        w |= ((w & mask) >> direction) & opponent
+        w |= ((w & mask) >> direction) & opponent
+        w |= ((w & mask) >> direction) & opponent
+        w |= ((w & mask) >> direction) & opponent
+        w |= ((w & mask) >> direction) & opponent
+        return (w & mask) >> direction
+
+
+def possible_moves(board, piece):
+    key = (board[0], board[1], piece)
+    if key in POSSIBLE_CACHE:
+        return POSSIBLE_CACHE[key]
+    else:
+        final = 0b0
+        possible = set()
+        for d in MASKS:
+            final |= fill(board[piece], board[not piece], d) & (FULL_BOARD - (board[piece] | board[not piece]))
+        while final:
+            b = final & -final
+            possible.add(POS[b])
+            final -= b
+        POSSIBLE_CACHE[key] = possible
+        return possible
+
+
+def place(b, piece, move):
+    board = {0: b[0], 1: b[1]}
+    board[piece] |= move
+
+    for i in MASKS:
+        c = fill(move, board[not piece], i)
+        if c & board[piece] != 0:
+            c = (c & MASKS[i * -1]) << i * -1 if i < 0 else (c & MASKS[i * -1]) >> i
+            board[piece] |= c
+            board[not piece] &= (FULL_BOARD - c)
+    return board
+
+
+def game_over(board, current):
+    if not (FULL_BOARD ^ (board[current]|board[not current])):
+        return True
+    player_moves = possible_moves(board, current)
+    opponent_moves = possible_moves(board, not current)
+    pm = len(player_moves)
+    return True if (pm|len(opponent_moves))==0 else (player_moves, pm)
+
+
+def pvs(board, depth, alpha, beta, piece):
+    state = game_over(board, piece)
+    if depth == 0 or state is True:
+        return hamming_weight(board[piece]) - hamming_weight(board[not piece])
+    current_moves, length = state
+    if length == 0:
+        val = pvs(board, depth, alpha, beta, not piece)
+        return [val[0], val[1:]+[-1]]
+    for pos, move in enumerate(current_moves):
+        if pos == 0:
+            val = pvs(place(board,piece, ), depth-1, -beta, -alpha, not piece)
+
+
+
+
+def minimax(board, piece, depth, alpha, beta, possible=[]):
+    """
+    Returns the best value, [sequence of the previous best moves]
+    """
+    key = (board[0], board[1], piece)
+    if key in TREE_CACHE:
+        return TREE_CACHE[key]
+
+    if not possible:
+        state = game_over(board, piece)
+        if state is True or depth == 0:
+            return hamming_weight(board[1]) - hamming_weight(board[0]), []
+        else:
+            current_moves, length = state
+
+        if length == 0:
+            val = minimax(board, not piece, depth, alpha, beta)
+            TREE_CACHE[key] = (val[0], val[1]+[-1])
+            return TREE_CACHE[key]
+    else:
+        current_moves = sorted(possible, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece)))
+    
+    best_opp_moves = []
+    if piece:
+        max_move, best_move = -100, 0
+        for i in current_moves:
+            placed = place(board, piece, MOVES[i])
+            tmp, opp_moves = minimax(placed, not piece, depth - 1, alpha, beta)
+            if tmp > max_move:
+                max_move, best_move, best_opp_moves = tmp, i, opp_moves
+            alpha = max(max_move, alpha)
+            if beta <= alpha:
+                break
+        TREE_CACHE[key] = (max_move, best_opp_moves + [best_move])
+        return TREE_CACHE[key]
+    else:
+        min_move, best_move = 100, 0
+        for i in current_moves:
+            placed = place(board, piece, MOVES[i])
+            tmp, opp_moves = minimax(placed, not piece, depth - 1, alpha, beta)
+            if tmp < min_move:
+                min_move, best_move, best_opp_moves = tmp, i, opp_moves
+            beta = min(min_move, beta)
+            if beta <= alpha:
+                break
+        TREE_CACHE[key] = (min_move, best_opp_moves + [best_move])
+        return TREE_CACHE[key]
+
+
+def actual_best_move(board, moves, piece):
+    val = minimax(board, piece, 12, -10000, 10000, possible=sorted(moves, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece))))
+    print("Min score: {0}; move sequence: {1}".format(val[0], val[1]) if piece else "Min score: {0}; move sequence: {1}".format(val[0]*-1, val[1]))
+
+def main():
+    string_board, piece = argv[1].upper(), argv[2].upper()
+    board = string_to_board(string_board)
+    piece = 0 if piece == 'O' else 1
+    possible = possible_moves(board, piece)
+    if possible:
+        actual_best_move(board, possible, piece)
+
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    print("{0}".format(time() - start))
--- games/othello/lab7/othello_bitboard.py
+++ games/othello/lab7/othello_bitboard.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 from sys import argv
 from time import time 
-from re import compile, IGNORECASE, match
-from functools import lru_cache
-
 
 FULL_BOARD = 0xffffffffffffffff
 RIGHT_MASK = 0xfefefefefefefefe
@@ -18,46 +15,21 @@ MASKS = {
     -7: LEFT_MASK,
     -9: RIGHT_MASK
 }
-STABLE_EDGE_REGEX = {
-    1: compile(r"^x+[o.]*x+$", IGNORECASE),
-    0: compile(r"^o+[x.]*o+", IGNORECASE)
-}
-PLAYER = {
-    'o': 0,
-    'O': 0,
-    'x': 1,
-    'X': 1,
-    '@': 1
-}
 
 
 MOVES = {i: 1 << (63 - i) for i in range(64)}
 POS = {MOVES[63 - i]: 63 - i for i in range(64)}
 
-bit_not = lambda x: FULL_BOARD ^ x
-is_on = lambda x, pos: x & MOVES[63-pos]
-binary_to_board = lambda board: "".join(['o' if is_on(board[0], 63-i) else 'x' if is_on(board[1],63-i) else '.' for i in range(64)])
-
-
-CORNERS = {0, 7, 56, 63}
-CORNER_NEIGHBORS = {1:63, 6:56, 8:63, 9:63, 14:56, 15:56, 48:7, 49:7, 54:0, 55:0, 57:7, 62:0}
-COL_EDGES =  {1:{7,15,23,31,39,47,55,63}, 0:{0,8,16,24,32,40,48,56}}
-ROW_EDGES = {0: {0,1,2,3,4,5,6,7}, 1:{56,57,58,59,60,61,62,6}}
-EDGES = {0: {0,1,2,3,4,5,6,7}, 1:{56,57,58,59,60,61,62,6}, 2:{7,15,23,31,39,47,55,63}, 3:{0,8,16,24,32,40,48,56}}
-
 
 HAMMING_CACHE = {}
 POSSIBLE_CACHE = {(68853694464, 34628173824, 0): {34, 43, 20, 29}, (68853694464, 34628173824, 1): {26, 19, 44, 37}}
 TREE_CACHE = {}
-GRADER_MOVE = [11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 57, 58, 61, 62, 63, 64, 65, 66, 67, 68, 71, 72, 73, 74, 75, 76, 77, 78, 81, 82, 83, 84, 85, 86, 87, 88]
-
 
 
 def string_to_board(board):
-    b = board.replace(".","0").replace("?","")
     return {
-        0: int(b.replace("o","1").replace("@","0"),2),
-        1: int(b.replace("o","0").replace("@","1"),2)
+        0: int(board.replace(".","0").replace("O","1").replace("X","0"),2),
+        1: int(board.replace(".","0").replace("O","0").replace("X","1"),2)
     }
 
 def hamming_weight(n):
@@ -181,80 +153,20 @@ def minimax(board, piece, depth, alpha, beta, possible=[]):
         return TREE_CACHE[key]
 
 
-def coin_heuristic(board, move, piece): # MAX: 100 MIN: -100
-    placed = place(board,piece,move)
-    num_player = hamming_weight(placed[piece])
-    num_opp = hamming_weight(placed[not piece])
-    return (num_player-num_opp)*20
+def actual_best_move(board, moves, piece):
+    val = minimax(board, piece, 12, -10000, 10000, possible=sorted(moves, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece))))
+    print("Min score: {0}; move sequence: {1}".format(val[0], val[1]) if piece else "Min score: {0}; move sequence: {1}".format(val[0]*-1, val[1]))
+
+def main():
+    string_board, piece = argv[1].upper(), argv[2].upper()
+    board = string_to_board(string_board)
+    piece = 0 if piece == 'O' else 1
+    possible = possible_moves(board, piece)
+    if possible:
+        actual_best_move(board, possible, piece)
 
 
-def mobility_heuristic(board, move, piece): # MAX: 0 MIN: -340
-    placed = place(board, piece, move)
-    opp_moves = possible_moves(placed, not piece)
-    h = -len(opp_moves)*40
-    if any(map(lambda x: x in CORNERS, opp_moves)):
-        h -= 100000
-    return h
-
-
-def next_to_corner(board, move, piece): # MAX: 150 MIN: -100000
-    """
-    Return 10 if next to a captured(own) corner
-    Return 0 if not next to a corner
-    Return -10 if next to an empty/taken(opponent) corner
-    """
-    if move not in CORNER_NEIGHBORS:
-        return 0
-    elif is_on(board[piece], CORNER_NEIGHBORS[move]):
-        return 600
-    return -1000000
-
-
-def stable_edge(board, move, piece):
-    h = 0
-    bs = binary_to_board(place(board, piece, move))
-    token = 'X' if piece else 'O'
-    for edge in EDGES:
-        if move not in EDGES[edge]:
-            continue
-        con = "".join([bs[x] for x in EDGES[edge]])
-        if con == token*8:
-            h =  3200
-        if STABLE_EDGE_REGEX[piece].match(con) is not None:
-            h =  2400
-    return h
-
-
-def a_good_move(board, moves, piece, empty):
-    for i in CORNERS:
-        if i in moves:
-            return i
-    
-    strat = coin_heuristic if empty <= 8 else mobility_heuristic
-    best = (-FULL_BOARD, 0)
-
-    for move in moves:
-        actual_move = MOVES[move]
-        h = strat(board, actual_move, piece)
-        h += next_to_corner(board, move, piece)
-        h += stable_edge(board, move, piece)
-
-        if h > best[0]:
-            best = (h, move)
-        elif h==best[0]:
-            tiebreaker = coin_heuristic if strat == mobility_heuristic else mobility_heuristic
-            move += tiebreaker(board, actual_move, piece) 
-            if h>best[0]:
-                best = (h, move)
-    return best[1]
-
-class Strategy:
-    def best_strategy(self, board, player, best_move, running):
-        board, piece = string_to_board(board), PLAYER[player]
-        moves = possible_moves(board, piece)
-        val = sorted(moves, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece)))
-        num_empty = FULL_BOARD ^ (board[0]|board[1])
-        if num_empty < 11:
-            best_move.value = GRADER_MOVE[minimax(board, piece, 12, -1000, 1000, moves)]
-        else:
-            best_move.value = GRADER_MOVE[a_good_move(board, moves, piece, num_empty)]
+if __name__ == "__main__":
+    start = time()
+    main()
+    print("{0}".format(time() - start))
