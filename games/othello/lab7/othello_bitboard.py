549fa8e209c807f3b530300435bc74a337931085
Anup Bagali <anup.bagali1@gmail.com>
2020-01-24 21:57:36 -0500

    70% on othello 7, can we do better?

--- games/othello/lab7/othello_bitboard.py
+++ games/othello/lab7/othello_bitboard.py
@@ -16,15 +16,25 @@ MASKS = {
     -7: LEFT_MASK,
     -9: RIGHT_MASK
 }
-
+WEIGHTS = [4, -3, 2, 2, 2, 2, -3, 4,
+          -3, -4, -1, -1, -1, -1, -4, -3,
+           2, -1, 1, 0, 0, 1, -1, 2,
+           2, -1, 0, 1, 1, 0, -1, 2,
+           2, -1, 0, 1, 1, 0, -1, 2,
+           2, -1, 1, 0, 0, 1, -1, 2,
+          -3, -4, -1, -1, -1, -1, -4,
+          -3, 4, -3, 2, 2, 2, 2, -3, 4
+          ]
 
 MOVES = {i: 1 << (63 - i) for i in range(64)}
 POS = {MOVES[63 - i]: 63 - i for i in range(64)}
+LOG = {MOVES[63-i]:i for i in range(64)}
 
 
 HAMMING_CACHE = {}
 POSSIBLE_CACHE = {(68853694464, 34628173824, 0): {34, 43, 20, 29}, (68853694464, 34628173824, 1): {26, 19, 44, 37}}
 TREE_CACHE = {}
+WEIGHT_CACHE = {}
 
 
 def string_to_board(board):
@@ -103,28 +113,37 @@ def game_over(board, current):
     opponent_moves = possible_moves(board, not current)
     pm = len(player_moves)
     om = len(opponent_moves)
-    return True if (pm|om) else (player_moves, pm, opponent_moves, om)
+    return True if (pm|om)==0 else (player_moves, pm, opponent_moves, om)
+
+
+def weight_table(board):
+    h = 0
+    while(board):
+        b = board&-board
+        h += WEIGHTS[LOG[b]]
+        board ^= b
+    return h
 
 
 def h(board, current, x_moves, x_length, o_moves, o_length):
-    h = hamming_weight(board[1]) - hamming_weight(board[0])
+    h = 20*x_length - 20*o_length
     x_corners = board[1] & CORNER_BOARD
     o_corners = board[0] & CORNER_BOARD
     if x_corners:
         h += 20*hamming_weight(x_corners)
     if o_corners: 
         h -= 20*hamming_weight(o_corners)
-    h += 10*x_length
-    h -= 10*o_length
+    h += hamming_weight(board[1])-hamming_weight(board[0])
+    h += weight_table(board[1])*20
+    h -= weight_table(board[0])*20
+    return h
 
 
 def minimax(board, piece, depth, alpha, beta, possible=[]):
     """
     Returns the best value, [sequence of the previous best moves]
     """
-    key = (board[0], board[1], piece, depth, alpha, beta)
-    if key in TREE_CACHE:
-        return TREE_CACHE[key]
+
 
     if not possible:
         state = game_over(board, piece)
@@ -132,19 +151,14 @@ def minimax(board, piece, depth, alpha, beta, possible=[]):
             return hamming_weight(board[1]) - hamming_weight(board[0]), []
         current_moves, length, opponent_moves, opponent_length = state
         if depth == 0:
-            x_moves, x_length = current_moves, length if piece else opponent_moves, opponent_length
-            o_moves, o_length = opponent_moves, opponent_length if piece else current_moves, length
-
-            return h(board, piece, x_moves, x_length, o_moves, o_length)
-        
-
+            x_moves, x_length = (current_moves, length) if piece else (opponent_moves, opponent_length)
+            o_moves, o_length = (opponent_moves, opponent_length) if piece else (current_moves, length)
+            return h(board, piece, x_moves, x_length, o_moves, o_length), []
         if length == 0:
             val = minimax(board, 1^piece, depth, alpha, beta)
-            TREE_CACHE[key] = (val[0], val[1]+[-1])
-            return TREE_CACHE[key]
+            return (val[0], val[1]+[-1])
     else:
-        current_moves = sorted(possible, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), 1^piece)))
-    
+        current_moves = possible
     best_opp_moves = []
     if piece:
         max_move, best_move = -100, 0
@@ -156,8 +170,7 @@ def minimax(board, piece, depth, alpha, beta, possible=[]):
             alpha = max(max_move, alpha)
             if beta <= alpha:
                 break
-        TREE_CACHE[key] = (max_move, best_opp_moves + [best_move])
-        return TREE_CACHE[key]
+        return (max_move, best_opp_moves + [best_move])
     else:
         min_move, best_move = 100, 0
         for i in current_moves:
@@ -168,11 +181,17 @@ def minimax(board, piece, depth, alpha, beta, possible=[]):
             beta = min(min_move, beta)
             if beta <= alpha:
                 break
-        TREE_CACHE[key] = (min_move, best_opp_moves + [best_move])
-        return TREE_CACHE[key]
+        return (min_move, best_opp_moves + [best_move])
 
 
-def actual_best_move(board, moves, piece):
+def midgame(board, moves, piece):
+    best = (-1000, 0) if piece else (1000, 0)
+    for depth in range(3, 40):
+        val = minimax(board, piece, depth, -10000, 10000, possible=sorted(moves, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece))))
+        best = max(val, best) if piece else min(val, best)
+        print("Min score: {0}; move sequence: {1}".format(best[0], best[1]) if piece else "Min score: {0}; move sequence: {1}".format(best[0]*-1, best[1]))
+
+def endgame(board, moves, piece):
     val = minimax(board, piece, 12, -10000, 10000, possible=sorted(moves, key=lambda x: len(possible_moves(place(board, piece, MOVES[x]), not piece))))
     print("Min score: {0}; move sequence: {1}".format(val[0], val[1]) if piece else "Min score: {0}; move sequence: {1}".format(val[0]*-1, val[1]))
 
@@ -181,8 +200,12 @@ def main():
     board = string_to_board(string_board)
     piece = 0 if piece == 'O' else 1
     possible = possible_moves(board, piece)
+    num_empty = hamming_weight(FULL_BOARD ^ (board[0]|board[1]))
     if possible:
-        actual_best_move(board, possible, piece)
+        if num_empty >= 14:
+            midgame(board, possible, piece)
+        else:
+            endgame(board, possible, piece)
 
 
 if __name__ == "__main__":
