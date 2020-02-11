b24fcbd34cb77d5afd6964ca110ede3855822eb2
Anup Bagali <anup.bagali1@gmail.com>
2020-02-11 11:16:51 -0500

    start placing words

--- crosswords/crosswords.py
+++ crosswords/crosswords.py
@@ -2,7 +2,7 @@
 import sys
 from re import compile, IGNORECASE, match
 
-SEED_REGEX = compile(r"(V|H)(\d*)x(\d*)(\w+|#)", IGNORECASE)
+SEED_REGEX = compile(r"(V|H)(\d*)x(\d*)(.+)", IGNORECASE)
 BLOCK = '#'
 EMPTY = '-'
 
@@ -12,6 +12,7 @@ BOARD = []
 HEIGHT, WIDTH, AREA, BLOCKS = 0,0,0,0
 SEEDS = []
 INDICES, INDICES_2D = {}, {}
+ROTATIONS = {}
 
 
 def to_string(pzl):
@@ -51,22 +52,30 @@ def brute_force(board, blocks):
     board[choice] = '-'
 
 def place_words():
-  global BOARD, SEEDS
+  global BOARD
+  for seed in SEEDS:
+    if seed[0] == 'H':
+      BOARD[INDICES_2D[(seed[1],seed[2])]:len(seed[3])] = seed[3]
+    if seed[0] == 'V':
+      idx = INDICES_2D[(seed[1],seed[2])]
+      for i in range(len(seed[3])):
+        BOARD[idx+(WIDTH*i)] = seed[3][i]
 
 
 def main():
-  global HEIGHT, WIDTH, SEEDS, FILE, BOARD, BLOCKS, INDICES, INDICES_2D
+  global HEIGHT, WIDTH, SEEDS, FILE, BOARD, BLOCKS, INDICES, INDICES_2D, ROTATIONS
   for arg in sys.argv[1:]:
     if 'H' == arg[0].upper() or 'V' == arg[0].upper():
       groups = match(SEED_REGEX, arg).groups()
-      SEEDS.append((groups[0], (int(groups[1]), int(groups[2])), groups[3].lower()))
+      SEEDS.append((groups[0].upper(), int(groups[1]), int(groups[2]), [*groups[3].lower()]))
     elif '.txt' in arg:
       FILE = arg
     elif arg.isdigit():
       BLOCKS = int(arg)
     else:
       HEIGHT, WIDTH = (int(x) for x in arg.split('x'))
-      AREA = HEIGHT*WIDTH
+  
+  AREA = HEIGHT*WIDTH
   INDICES = {index: (index // WIDTH, (index % WIDTH)) for index in range(AREA)}
   INDICES_2D = {(i, j): i * WIDTH + j for i in range(HEIGHT) for j in range(WIDTH)}
   
@@ -76,6 +85,9 @@ def main():
   
   BOARD = [*EMPTY*AREA]
   place_words()
+  print(to_string(BOARD))
+  print(BOARD)
+  return
 
   if BLOCKS == 0:
     print(to_string(BOARD))
