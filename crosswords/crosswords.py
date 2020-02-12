0daf8dab81e3906e9463758f0c360843e01b887d
Anup Bagali <anup.bagali1@gmail.com>
2020-02-11 20:53:17 -0500

    fixed block placing

--- crosswords/crosswords.py
+++ crosswords/crosswords.py
@@ -13,6 +13,7 @@ HEIGHT, WIDTH, AREA, BLOCKS = 0,0,0,0
 SEEDS = []
 INDICES, INDICES_2D = {}, {}
 ROTATIONS = {}
+NEIGHBORS = {}
 
 
 def to_string(pzl):
@@ -22,34 +23,6 @@ def to_string(pzl):
 def rotate180(board, pivot):
     return board[INDICES_2D[(WIDTH - pivot[0] - 1, HEIGHT - pivot[1] - 1)]]
 
-
-def is_invalid(board, remaining_blocks):
-  if remaining_blocks == 0:
-    for pos in range(AREA):
-      if board[pos] != rotate180(board, INDICES[pos]):
-        return False
-  
-  if "#"*WIDTH in board:
-    return False
-  
-
-
-
-def brute_force(board, blocks):
-  if not blocks:
-    return board
-  
-  if is_invalid(board, blocks):
-    return False
-  
-  set_of_choices = [pos for pos, elem in enumerate(board) if elem == '-']
-
-  for choice in set_of_choices:
-    board[choice] = '#'
-    b_f = brute_force(board, blocks-1)
-    if b_f:
-      return b_f
-    board[choice] = '-'
 
 def place_words():
   global BOARD
@@ -63,7 +36,7 @@ def place_words():
 
 
 def main():
-  global HEIGHT, WIDTH, SEEDS, FILE, BOARD, BLOCKS, INDICES, INDICES_2D, ROTATIONS
+  global HEIGHT, WIDTH, SEEDS, FILE, BOARD, BLOCKS, INDICES, INDICES_2D, ROTATIONS, NEIGHBORS
   for arg in sys.argv[1:]:
     if 'H' == arg[0].upper() or 'V' == arg[0].upper():
       groups = match(SEED_REGEX, arg).groups()
@@ -76,24 +49,28 @@ def main():
       HEIGHT, WIDTH = (int(x) for x in arg.split('x'))
   
   AREA = HEIGHT*WIDTH
-  INDICES = {index: (index // WIDTH, (index % WIDTH)) for index in range(AREA)}
-  INDICES_2D = {(i, j): i * WIDTH + j for i in range(HEIGHT) for j in range(WIDTH)}
+  INDICES = {index: (index // WIDTH, (index % WIDTH)) for index in range(AREA)} # idx -> (row, col)
+  INDICES_2D = {(i, j): i * WIDTH + j for i in range(HEIGHT) for j in range(WIDTH)} # (row, col) -> idx
   
   if BLOCKS == AREA:
-    print(to_string(BLOCK*AREA))
-    return
+    return to_string(BLOCK*AREA)
   
   BOARD = [*EMPTY*AREA]
   place_words()
-  print(to_string(BOARD))
-  print(BOARD)
-  return
 
   if BLOCKS == 0:
-    print(to_string(BOARD))
-    return
+    return to_string(BOARD)
+
+  for index in range(0, AREA):  # saves all possible neighbors for all indices
+    row = index // WIDTH
+    neighbors = [i for i in [index + WIDTH, index - WIDTH] if 0 <= i < AREA]
+    if (index + 1) // WIDTH == row and index + 1 < AREA:
+        neighbors.append(index + 1)
+    if (index - 1) // WIDTH == row and index - 1 >= 0:
+        neighbors.append(index - 1)
+    NEIGHBORS[index] = neighbors
   
 
 
 if __name__ == '__main__':
-  main()
+  print(main())
