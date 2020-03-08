fd24c134e8e3cfa52add473ddc8819f62270b5f6
Anup Bagali <anup.bagali1@gmail.com>
2020-03-07 22:31:03 -0500

    start networks

---
+++ networks/lab1/network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import random
+
+
+N = 1e5
+
+
+def populate(nodes):
+    pass
+
+
+def main():
+     nodes = {n: set() for n in range(N)}
+
+
+
+
+
+if __name__ == "__main__":
+    main()
