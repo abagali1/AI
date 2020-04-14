e9ed29a5cdb081bb26d54f03ec311b183f948a65
Anup Bagali <anup.bagali1@gmail.com>
2020-04-14 15:49:02 -0400

    idk why i added docstrings

--- networks/feedforward/ff.py
+++ networks/feedforward/ff.py
@@ -10,34 +10,50 @@ TRANSFERS = {
 }
 
 
-def read_weights(file, in_nodes):
-    weights, count = [], 0
-    file = open(file).read().splitlines()
+def read_weights(file: str, in_nodes: int) -> tuple:
+    """
+    Takes in a filename and amount of input nodes and returns a tuple of layers and amount of layers
+    :param file: file to read weights from
+    :param in_nodes: amount of input nodes
+    :return: ([layers], amount of layers)
+    """
+    weights, count, file = [], 0,  open(file).read().splitlines()
     for line in file[:-1]:
         w = [float(x) for x in line.split(" ")]
         amt = len(w)
         out_nodes = amt // in_nodes
         weights.append([w[i:i + in_nodes] for i in range(0, amt, in_nodes)])
-        in_nodes = out_nodes
-        count += 1
+        in_nodes, count = out_nodes, count + 1
     weights.append([[float(x)] for x in file[-1].split(" ")])
     return weights, count
 
 
-def dot(x, y):
+def dot(x: list, y: list) -> float:
+    """
+    Returns the dot product of two matrices, assumes matrices are of equal dimension
+    :param x: First matrix
+    :param y: Second matrix
+    :return: dot product of two matrices
+    """
     return sum([x[i] * y[i] for i in range(len(y))])
 
 
-def feedforward(inputs, layers, tf):
+def feedforward(inputs: list, layers: tuple, tf: callable) -> list:
+    """
+    Iteratively feeds inputs through neural network with layers and transfer function tf
+    :param inputs: initial inputs
+    :param layers: neural network layers
+    :param tf: transfer function to use
+    :return: list of final values outputted after feedforward
+    """
     for pos, layer in enumerate(layers[0]):
         inputs = [tf(x) for x in [dot(inputs, weights) for weights in layer]] if pos < layers[1] else [dot([inputs[i]], weight) for i, weight in enumerate(layer)]
     return inputs
 
 
 def main():
-    tf, inputs = TRANSFERS[argv[2]], [*map(float, argv[3:])]
-    layers = read_weights(argv[1], len(inputs))
-    print(' '.join([*map(str, feedforward(inputs, layers, tf))]))
+    inputs = [*map(float, argv[3:])]
+    print(' '.join([*map(str, feedforward(inputs, read_weights(argv[1], len(inputs)), TRANSFERS[argv[2]]))]))
 
 
 if __name__ == "__main__":
