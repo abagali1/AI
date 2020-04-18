2b3c1c274df0fb0071a2e67702102cc6a93345bc
Anup Bagali <anup.bagali1@gmail.com>
2020-04-17 23:57:16 -0400

    organization does not converge

--- networks/k-means/kmeans.py
+++ networks/k-means/kmeans.py
@@ -3,6 +3,7 @@ import io
 import random
 from sys import argv
 from PIL import Image
+from time import time
 from urllib import request
 
 
@@ -10,12 +11,27 @@ def dist(r, m):
     return (r[0] - m[0]) ** 2 + (r[1] - m[1]) ** 2 + (r[2] - m[2]) ** 2
 
 
-def organize(means, pixels):
-    new_means = {}
-    for mean, pixs in pixels.items():
-        m = (sum(r[0] * r[3] for r in pixs[1]) / pixs[0], sum(g[1] * g[3] for g in pixs[1]) / pixs[0],
-             sum(b[2] * b[3] for b in pixs[1]) / pixs[0])
-        new_means[m] = pixels[mean]
+def organize(previous_pixels, all_pixels):
+    new_means, count = {}, 0
+    for mean, pixs in previous_pixels.items():
+        m = [0, 0, 0]
+        for r, g, b, count in pixs[1]:
+            m[0] += r * count
+            m[1] += g * count
+            m[2] += b * count
+        m = (m[0] / pixs[0], m[1] / pixs[0], m[2] / pixs[0])
+
+        new_means[m] = previous_pixels[mean]
+
+    for pixel in all_pixels:
+        new_mean, tmp = min(new_means.keys(), key=lambda m: dist(pixel, m)), (*pixel, all_pixels[pixel])
+        current_mean = [x for x in new_means if tmp in new_means[x][1]][0]
+        if new_mean != current_mean:
+            new_means[current_mean][1].remove(tmp)
+            new_means[new_mean][1].add(tmp)
+            count += 1
+    print("Moved: {}".format(count))
+    return count == 0, new_means
 
 
 def kmeans(k, image):
@@ -34,15 +50,31 @@ def kmeans(k, image):
             kpixels[key][1].add((*pixel, all_pixels[pixel]))
         else:
             kpixels[key] = [all_pixels[pixel], {(*pixel, all_pixels[pixel])}]
-    print(kpixels)
-    organize(means, kpixels)
+    gen = 0
+    while True:
+        print("Gen: {}".format(gen))
+        o = organize(kpixels, all_pixels)
+        if o[0]:
+            return kpixels
+        else:
+            kpixels = o[1]
+        gen += 1
+
+# def reconstruct(image, means):
+#     w, h = image.size()
+#     pix = image.load()
+#     for x in range(w):
+#         for y in range(h):
+#             new_val = [x ]
 
 
 def main():
-    random.seed(1)
+    start = time()
     k, img = int(argv[1]), argv[2]
     img = Image.open(io.BytesIO(request.urlopen(img).read())) if "http" in img else Image.open(open(img, 'rb'))
-    kmeans(k, img)
+    kmeanified = kmeans(k, img)
+    print(time()-start)
+    # reconstruct(img, kmeanified)
 
 
 if __name__ == "__main__":
