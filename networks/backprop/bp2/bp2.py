8bf25d9eeabfb6ae7f02d5518a482fe9edaec2a0
Anup Bagali <anup.bagali1@gmail.com>
2020-05-19 17:53:49 -0400

    update weights

--- networks/backprop/bp2/bp2.py
+++ networks/backprop/bp2/bp2.py
@@ -32,9 +32,9 @@ def parse_args(s):
 
 def construct_network(in_nodes):
     weights = [
-        [[random() for _ in range(in_nodes + 1)] for _ in range(4)],
-        [[random() for _ in range(4)] for _ in range(2)],
-        [[random() for _ in range(2)]],
+        [[random() for _ in range(in_nodes + 1)] for _ in range(7)],
+        [[random() for _ in range(7)] for _ in range(3)],
+        [[random() for _ in range(3)]],
         [[random()]]
     ]
     return weights, len(weights) - 1
