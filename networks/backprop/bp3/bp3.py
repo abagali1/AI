1b14dce60bcb3797ea7217dec5e23fd75189a92b
Anup Bagali <anup.bagali1@gmail.com>
2020-05-26 18:13:04 -0400

    compress combining code into 1 for loop

--- networks/backprop/bp3/bp3.py
+++ networks/backprop/bp3/bp3.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 from sys import argv
-from random import random, seed
 
 
 def parse_args():
-    ineq, val = "", ""
+    val = ""
     for char in argv[2][7:]:
         if char.isdigit() or char == ".":
             val += char
-        else:
-            ineq += char
     lines = open(argv[1]).read().splitlines()
     weights = []
     for line in lines:
@@ -21,7 +18,7 @@ def parse_args():
                 continue
         if tmp:
             weights.append(tmp)
-    return weights, ineq, float(val)
+    return weights, float(val)
 
 
 def construct_squaring_network(m, in_nodes=2):
@@ -32,40 +29,36 @@ def construct_squaring_network(m, in_nodes=2):
         out_nodes = amt // in_nodes
         weights.append([w[i:i + in_nodes] for i in range(0, amt, in_nodes)])
         in_nodes, count = out_nodes, count + 1
-    return weights, count
+    return weights
 
 
-def derive_network(n, r, ineq):
+def derive_network(n, r):
+    print("R: "+str(r))
     weights = []
-    for pos, x in enumerate(n[0][:-1]):
-        layer = []
-        if pos == 0:
-            nodes = len(x)
-            for y in range(nodes+nodes):
-                layer_weights = x[y % nodes]
+    for pos, x in enumerate(n[:-1]):
+        layer, nodes = [], len(x)
+        for y in range(nodes+nodes):
+            layer_weights = x[y % nodes]
+            if pos == 0:  # combine initial weights
                 if y < nodes:
-                    layer.append([layer_weights[0], 0, layer_weights[1]])
+                    layer.append([layer_weights[0] / r, 0, layer_weights[1]])
                 else:
-                    layer.append([0, layer_weights[0], layer_weights[1]])
-        else:
-            nodes = len(x)
-            for y in range(nodes+nodes):
-                layer_weights = x[y % nodes]
+                    layer.append([0, layer_weights[0] / r, layer_weights[1]])
+            else:  # combine other layers
                 if y < nodes:
-                    layer.append([*layer_weights, *[0 for _ in layer_weights]])
+                    layer.append([*layer_weights, *[0 for _ in layer_weights]])  # weights for top "x-squarer" network
                 else:
-                    layer.append([*[0 for _ in layer_weights], *layer_weights])
+                    layer.append([*[0 for _ in layer_weights], *layer_weights])  # weights for bottom "y-squarer" network
         weights.append(layer)
-    weights.append([n[0][-1][0]*2])
-    weights.append([[0.68394]])  # TODO: Figure out determining weight
+    weights.append([n[-1][0]*2])  # network addition weights
+    weights.append([[0.68394]])  # final weight
     return weights
 
 
 def main():
-    seed(1738114)
-    file_weights, inequal, val = parse_args()
+    file_weights, val = parse_args()
     square_x = construct_squaring_network(file_weights)
-    network = derive_network(square_x, val, inequal)
+    network = derive_network(square_x, val)
     print("Layer Counts: {} {}".format(2 + 1, ' '.join(str(len(x)) for x in network)))
     print(
         '\n'.join(map(str, ([', '.join(map(str, weights)) for weights in layer] for layer in network)))
