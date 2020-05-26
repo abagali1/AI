28c58a1ab4bdd0a5ec2e5fe0c1f55ed13c6dcfb8
Anup Bagali <anup.bagali1@gmail.com>
2020-05-26 00:56:50 -0400

    read in squaring network

--- networks/backprop/bp3/bp3.py
+++ networks/backprop/bp3/bp3.py
@@ -1,79 +1,48 @@
 #!/usr/bin/env python3
-from math import exp
 from sys import argv
-from random import random, seed
-
-ALPHA = 0.15
-DATASET_LENGTH = 100000
-EPOCHS = 5
-
-
-def tf(x):
-    return 1 / (1 + exp(-x))
-
-
-def derivative(x):
-    return x * (1 - x)
-
-
-def dot(x: list, y: list) -> float:
-    return sum(i * j for i, j in zip(x, y))
+from random import seed
 
 
 def parse_args():
-    return argv[1]
+    ineq, val = "", ""
+    for char in argv[2][7:]:
+        if char.isdigit() or char == ".":
+            val += char
+        else:
+            ineq += char
+    lines = open(argv[1]).read().splitlines()
+    weights = []
+    for line in lines:
+        nums, tmp = line.replace(',', '').replace('[', '').replace(']', '').split(" "), []
+        for num in nums:
+            try:
+                tmp.append(float(num))
+            except ValueError:
+                continue
+        if tmp:
+            weights.append(tmp)
+    return weights, ineq, val
 
 
-def construct_network(filename, in_nodes):
-    weights = [
-        [[random() for _ in range(in_nodes + 1)] for _ in range(4)],
-        [[random() for _ in range(4)] for _ in range(2)],
-        [[random() for _ in range(2)]],
-        [[random()]]
-    ]
-    return weights, len(weights) - 1
+def construct_squaring_network(m, in_nodes=2):
+    weights, count = [], 0
+    for line in m[:-1]:
+        w = [float(x) for x in line]
+        amt = len(w)
+        out_nodes = amt // in_nodes
+        weights.append([w[i:i + in_nodes] for i in range(0, amt, in_nodes)])
+        in_nodes, count = out_nodes, count + 1
+    weights.append([[float(x)] for x in m[-1]])
+    return weights, count
 
 
-def feedforward(layers, inputs):
-    values = [inputs]
-    for i in range(layers[1]):
-        inputs = [tf(dot(inputs, weights)) for weights in layers[0][i]]
-        values.append(inputs)
-    return values
-
-
-def backprop(layers, ff_outputs, t):
-    x, w = ff_outputs[-1][0], layers[0][-1][0][0]
-    grad = [[[(t - (x * w)) * x]]]
-    prev_error = [(t - (x * w)) * w * derivative(x)]
-
-    for ff_in, layer in zip(ff_outputs[:-1][::-1], layers[0][:-1][::-1]):
-        grad.append([[x * e for x in ff_in] for e in prev_error])
-        error = [0 for _ in range(len(layer[0]))]
-        for pos, weights in enumerate(layer):
-            for p2, weight in enumerate(weights):
-                error[p2] += (weight * prev_error[pos]) * derivative(ff_in[p2])
-        prev_error = error
-    return grad[::-1]
-
-
-def update_weights(network, grad):
-    for layer_idx in range(len(network[0])):
-        for weights_idx in range(len(network[0][layer_idx])):
-            for weight_idx in range(len(network[0][layer_idx][weights_idx])):
-                network[0][layer_idx][weights_idx][weight_idx] += grad[layer_idx][weights_idx][weight_idx] * ALPHA
-    return network
-
-
-def train(network, s):
-    print('\n'.join(map(str, ([', '.join(map(str, weights)) for weights in layer] for layer in network[0]))).replace("'", ""), '\n')
 
 
 def main():
     seed(1738114)
-    filename = parse_args()
-    network = construct_network(2)
-    print("Layer Counts: {} {}".format(2 + 1, ' '.join(str(len(x)) for x in network[0])))
+    file_weights, inequal, val = parse_args()
+    square_x = construct_squaring_network(file_weights)
+    # square_y = ([[y.copy() for y in x] for x in square_x[0]], square_x[1])
 
 
 if __name__ == "__main__":
