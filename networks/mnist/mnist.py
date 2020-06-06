83915e0e405f94712f5a0953a038fe7c4f38fb4a
Anup Bagali <anup.bagali1@gmail.com>
2020-06-05 21:51:55 -0400

    initial mnist

--- networks/mnist/mnist.py
+++ networks/mnist/mnist.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
-import sys
 import torch
-from torchvision import datasets
+
+ALPHA = 0.1
+EPOCHS = 100000
+BATCH_SIZE = 128
 
 
-def read_data(**kwargs):
-    return datasets.MNIST(".", download=True, **kwargs)
+def read_data(filename):
+    lines = open(filename).read().splitlines()
+    in_val, out_val = [], []
+    for line in lines:
+        parts = line.split(',')
+        in_val.append([float(x) for x in parts[1:]])
+        out_val.append([0.0 if x == int(parts[0]) else 1.0 for x in range(10)])
+    return torch.tensor(in_val), torch.tensor(out_val)
+
+
+def create_network():
+    return torch.nn.Sequential(
+        *[
+            torch.nn.Linear(784, 300, bias=True),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(300, 100, bias=False),
+            torch.nn.Sigmoid(),
+        ],
+        torch.nn.Linear(100, 10, bias=False)
+    )
 
 
 def main():
-    training = read_data(train=True)
-    testing = read_data(train=False)
+    network = create_network()
+    train_in, train_out = read_data('mnist_train.csv')
+    # test_data = read_data(False)
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(network.parameters(), lr=ALPHA)
+    for epoch in range(EPOCHS + 1):
+        y = network(train_in)
+        loss = criterion(y, train_out)
+        if not epoch % 500 or epoch < 10:
+            print('epoch: ', epoch, ' loss: ', loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    torch.save(network.state_dict(), "mnist_model.torch")
 
 
 if __name__ == "__main__":
